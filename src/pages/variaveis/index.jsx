d067027201e3dbb165a0c451d2b10479c978c2ff
Guilherme Pianoski <ra52784596893@acaonsfatima.org.br>
2024-09-09 23:25:28 -0300

    mais váriaveis

--- src/pages/variaveis/index.jsx
+++ src/pages/variaveis/index.jsx
@@ -3,6 +3,11 @@ import { useState } from 'react';
 
 export default function Variavel() {
     const [Contador, setContador] = useState(0);
+    const [tituloS2, setTituloS2] = useState('Oie');
+    const [tituloS3, setTituloS3] = useState('Escolha um item');
+    const [marcouOpcaoS4, setMarcouOpcaoS4] = useState(false);
+    const [tituloS5, setTituloS5] = useState('Oie');
+    const [descricaoS5, setDescricaoS5] = useState('Oiii');
 
     //let Contador = 0
         function aumentar() {
@@ -19,6 +24,30 @@ export default function Variavel() {
             } 
        }
 
+       function alterarTituloS2(e) {
+         let novoValor = e.target.value;
+         setTituloS2(novoValor);
+       }
+
+       function alterarTituloS3(e) {
+        let novoValor = e.target.value;
+        setTituloS3(novoValor);
+      }
+
+      function alterarOpcaos4(e) {
+        let novoValor = e.target.value.checked;
+        setMarcouOpcaoS4(novoValor)
+      }
+
+      function alterarDescricaoS5(e) {
+        let novoValor = e.target.value;
+        setDescricaoS5(novoValor);
+      }
+
+      function alterarTituloS5() {
+         setTituloS5(descricaoS5);
+      }
+
 
     return(
         <div className='pagina-variaveis pagina'>
@@ -33,9 +62,41 @@ export default function Variavel() {
                   <button onClick={aumentar}>+</button>
                   {Contador}
                   <button onClick={diminuir}>-</button>
-                  </div>
-
+                </div>
             </div>
+
+            <div className='secao'>
+                <h1> {tituloS2} </h1>
+                <input type='text' value={tituloS2} onChange={alterarTituloS2} />
+            </div>
+
+            <div className='secao'>
+                <h1> {tituloS3} </h1>
+                <select onChange={alterarTituloS3}>
+                    <option>Selecione</option>
+                    <option>Javascript</option>
+                    <option>Html/Css</option>
+                    <option>ReactJS</option>
+                </select>
+            </div>
+
+        
+            <div className='secao'>
+                <h1> Programar é lindo? {marcouOpcaoS4 ? 'Sim' : 'Não'} </h1>
+                <input type='checkbox' checked={marcouOpcaoS4} onChange={alterarOpcaos4} /> Programar é lindo?
+            </div>
+            
+            <div className='secao'>
+                <h1> {tituloS5} </h1>
+                <input type='text' value={descricaoS5} onChange={alterarDescricaoS5} />
+
+                <button onClick={alterarTituloS5}>Alterar</button>
+            </div>
+
+
         </div>
     )
 }
+
+
+//'?' = "Se"
